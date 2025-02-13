7602d9a548f8faaf6025b58c749134be43fdbf0a
Nithursan <nithursan.muraleetharan@gmail.com>
2025-02-13 14:47:01 +0000

    Refactor more

--- engine_python/minimax.py
+++ engine_python/minimax.py
@@ -27,14 +27,32 @@ def calculateBoardMaterial(board: chess.Board):
 def evaluate(board: chess.Board):
     return calculateBoardMaterial(board)
 
+def minimax(board: chess.Board, depth: int, alpha: int, beta: int, isMax: bool):
+    if (depth == 0):
+        return(evaluate(board))
+    
+    moves = list(board.legal_moves)
+    
+    if(isMax):
+        maxEval = float('-inf')
+        for move in moves:
+            board.push_san(move)
+            eval = minimax(board, depth - 1, alpha, beta, False)
+            
+        
+
 def main():
     board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
     game_board = display.start()
+    print("Advantage: " + str(evaluate(board)))
+    print(board.legal_moves)
+    print(float('-inf'))
+    print(float('inf'))
     while True:
         display.check_for_quit()
         display.update(board.fen(), game_board)
         
-    # print("Advantage: " + str(evaluate(board)))
+    
     
 if __name__ == "__main__":
     main()
